61726cc9b1738d896777e3947a78b32952d55eca
Crowdbotics <team@crowdbotics.com>
2022-05-09 20:53:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sentrendmobile_35148",
+  url: "https://sentrendmobile_35148.botics.co",
+  api: "https://sentrendmobile_35148.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
